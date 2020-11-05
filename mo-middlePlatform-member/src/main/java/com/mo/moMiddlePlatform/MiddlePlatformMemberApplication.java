22225a84171ed37c766a1574e9875e6378b00357
moweiquan <295597613@qq.com>
2020-11-05 18:24:04 +0800

    中台-会员模块创建

---
+++ mo-middlePlatform-member/src/main/java/com/mo/moMiddlePlatform/MiddlePlatformMemberApplication.java
@@ -0,0 +1,15 @@
+package com.mo.moMiddlePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2020/11/5
+ */
+@SpringBootApplication
+public class MiddlePlatformMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlePlatformMemberApplication.class, args);
+    }
+}
---
+++ mo-middlePlatform-member/src/main/java/com/mo/moMiddlePlatform/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.mo.moMiddlePlatform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mo on 2020/11/5
+ */
+@RestController
+public class MemberController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(String name) {
+        return "欢迎来到middle platform " + name;
+    }
+}
