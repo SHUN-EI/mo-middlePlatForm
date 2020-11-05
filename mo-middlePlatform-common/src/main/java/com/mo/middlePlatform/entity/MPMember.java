ca7a14876d7e479976dd144204439400297ed457
moweiquan <295597613@qq.com>
2020-11-06 00:39:04 +0800

    中台-公共模块创建

---
+++ mo-middlePlatform-common/src/main/java/com/mo/middlePlatform/entity/MPMember.java
@@ -0,0 +1,40 @@
+package com.mo.middlePlatform.entity;
+
+import java.util.Date;
+
+/**
+ * Created by mo on 2020/11/5
+ * 会员
+ */
+public class MPMember {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String phone;
+
+    private Integer age;
+
+    private Integer gender;
+
+    private String icon;
+
+    private String city;
+
+    private String personalizedSign;
+
+    private Date birthday;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+}
